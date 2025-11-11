f960af98104e597433811aff5fd956aaee4fa621
TheZhe <42720670+thezhe@users.noreply.github.com>
2025-11-11 01:30:05 -0500

    Minimize

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# dotfiles-arch
+# dotfiles
 
-Arch AMD cpu dotfiles
+amd-arch dotfiles
--- install.sh
+++ install.sh
@@ -1,35 +1,25 @@
 #!/usr/bin/env sh
 set -eu
-install_script_dir="$(cd -- "$(dirname -- "${0}")" && pwd -P)"
-## pacman
-sudo pacman -Syu
-sudo pacman -S \
-# firmware
-amd_ucode \
-# GNOME
-gdm gnome-control-center gnome-logs \
-# base
-firefox gnome-disk-utility keepassxc less ufw \
-# audio
-easyeffects rhythmbox qtractor \
-# dev
-base-devel cmake git osv-scanner shellcheck shfmt
-## systemctl
-sudo systemctl mask --now avahi-daemon.service avahi-daemon.socket avahi-dnsconfd.service bluetooth.service bluetooth.target iptables.service ip6tables.service passim.service printer.target ssh-access.target sshd.service sshd@.service sshd-unix-local.socket
-sudo systemctl enable --now gdm ufw
-## ufw
-sudo ufw enable
-## yay
 (
-cd /tmp
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
+	# shellcheck disable=SC2312
+	cd "$(cd -- "$(dirname -- "${0}")" pwd -P)"
+	# pacman
+	sudo pacman -Syu
+	sudo pacman -S amd_ucode gdm gnome-control-center gnome-logs firefox gnome-disk-utility keepassxc less ufw base-devel cmake git shellcheck shfmt
+	# systemctl
+	sudo systemctl mask --now avahi-daemon.service avahi-daemon.socket avahi-dnsconfd.service bluetooth.service bluetooth.target iptables.service ip6tables.service passim.service printer.target ssh-access.target sshd.service sshd@.service sshd-unix-local.socket
+	sudo systemctl enable --now gdm ufw
+	# ufw
+	sudo ufw enable
+	# yay
+	(
+		cd /tmp
+		git clone https://aur.archlinux.org/yay-bin.git
+		cd yay-bin
+		makepkg -si
+	)
+	yay -S visual-studio-code-bin
+	# rootfs
+	sudo cp -frT rootfs /
+	printf 'Restart to apply changes\n'
 )
-yay -S visual-studio-code-bin
-## rootfs
-sudo cp -frT "${install_script_dir}/rootfs" /
-## HOME
-cp -frT "${install_script_dir}/home" "${HOME}"
-## Epilogue
-printf 'Restart to apply changes\n'
